3e201d67f908bb6bd5f5abfb11b1520f3cf202b8
sando2004 <s.mugwaneza@alustudent.com>
2025-07-16 04:48:49 -0700

    finding out number of subscribers

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-""" Function that returns the number of subscribers for a subreddit """
+"""Module for querying subreddit subscriber count using Reddit API"""
 import requests
-
+#defining number of subscribers
 
 def number_of_subscribers(subreddit):
-    """Returns total number of subscribers for a given subreddit"""
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    """Returns the number of subscribers for a given subreddit"""
+    if not isinstance(subreddit, str):
+        return 0
+
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'ALUStudentBot/1.0'}
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            return 0
-    except Exception:
+            return response.json().get('data', {}).get('subscribers', 0)
+        return 0
+    except requests.RequestException:
         return 0
---
+++ api_advanced/README.md
@@ -0,0 +1,1 @@
+Working on advanced API and finding out the number of subscribers for the first file
